fb130c44919fa7fcacbaa3e55103285bbd1f1d63
maneetpaul <chawlamaneet@gmail.com>
2019-03-09 23:38:32 -0500

    Replaced switch with else if

--- main.js
+++ main.js
@@ -1,8 +1,60 @@
-function myFunction() {
-  alert("The form was submitted");
+function runQuestion () {
+
+// Takes HTML text input and defines userQuestion var value  
+let userQuestion = document.getElementById("questionText").value; 
+    
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+if (randomNumber === 0) {
+  	eightBall = 'It is certain';
+} else if (randomNumber === 1) {
+    eightBall = 'It is decidedly so';
+} else if (randomNumber === 2) {
+  	eightBall = 'Reply hazy try again';
+} else if (randomNumber === 3) {
+  	eightBall = 'Cannot predict now';
+} else if (randomNumber === 4) {
+  	eightBall = 'Do not count on it';
+} else if (randomNumber === 5) {
+  	eightBall = 'My sources say no';
+} else if (randomNumber === 6) {
+  	eightBall = 'Outlook not so good';
+} else if (randomNumber === 7) {
+	eightBall = 'Signs point to yes';
+}
 
 
+/* Why doesn't this work but else if does?
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+*/
 
-// let randomNumber = Math.floor(Math.random() * 8);
+// Displays eightBall text into HTML
+document.getElementById("answerBox").innerHTML = eightBall;
 
-
+};
